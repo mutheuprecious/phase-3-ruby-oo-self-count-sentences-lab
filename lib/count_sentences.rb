1652369ba17fc6f2d5ca558524e0a2fddb39fc64
Precious <preciousmaregwa1997@gmail.com>
2022-06-12 17:44:33 +0300

    commit tree f0f137040048503e3f7e995a0608f5d2a0dc5514

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,29 @@
+
+
+
 require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(/[.?!]/)
+    array.each do |word|
+      if word == ""
+        array.delete(word)
+      end
+    end
+    array.size
   end
 end
